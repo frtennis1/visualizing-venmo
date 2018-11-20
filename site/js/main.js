53fa79e5f1ec08b5d023e83f58eb37837753b611
David Miller <davidjackmiller@gmail.com>
2018-11-19 21:34:40 -0500

    completed section for how to change venmo privacy settings

--- site/js/main.js
+++ site/js/main.js
@@ -22,8 +22,6 @@ function dataLoaded(error, _users, _transactions, _labeledTransactions, _wordCou
     // Create local transaction breakdown pie chart
     localTransactionBreakdown = new PieChart("transaction-breakdown-local", _labeledTransactions);
 
-    localTransactionBreakdown.filterForUser(183704);
-
     // Create word cloud
     var wordcloud = new WordCloud("word-cloud", _wordCount);
 
@@ -71,3 +69,37 @@ function userFilter() {
     localTransactionBreakdown.filterForUser(chosenUserId);
 
 }
+
+/*
+    Handling for How To Section
+ */
+
+var currentHowTo = 0;
+var slides = [
+    "<h3>When you open Venmo, it should look a little like this. This is your home screen. Press the <span class='highlight'>menu</span> button in the top left to open the menu.</h3>",
+    "<h3>A sidebar will open up on top of your home page. Click the <span class='highlight'>Settings</span> button.</h3>",
+    "<h3>Click on the <span class='highlight'>Privacy</span> button to open up your privacy settings.</h3>",
+    "<h3>Click on the <span class='highlight'>Private</span> button to make all future Venmo transactions private by default. Don't forget to change your past transactions too thought!</h3>",
+    "<h3>Click the <span class='highlight'>Past Transactions</span> button to retroactively change the privacy settings of past transactions.</h3>",
+    "<h3>Click the <span class='highlight'>Change All to Private</span> button to instantly protect your entire Venmo history. That's it! Venmo will no longer serve your transactions through their public API.</h3>"
+];
+// Function called when the user clicks on the next button in the how to section
+function howToButtonNext() {
+    currentHowTo += 1;
+    howToButtonPress();
+}
+function howToButtonPrev() {
+    currentHowTo -= 1;
+    howToButtonPress();
+}
+function howToButtonPress() {
+    d3.select("#howToImage").style("opacity", 0).attr("src", "img/howto/howto"+currentHowTo+".PNG");
+    d3.select("#howToImage").transition().duration(700).ease(d3.easeLinear).style("opacity", 1);
+    // Disable the previous button if on the first page
+    d3.select("#howToPrev").classed("disabled", (currentHowTo == 0));
+    // Have the next button say start over if on the last page
+    d3.select("#howToNext").classed("disabled", (currentHowTo == slides.length - 1));
+    // Update the instructions
+    d3.select("#howToText").html(slides[currentHowTo]);
+}
+howToButtonPress();
